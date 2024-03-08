f97cedf4eee3470e0be0a425ec72c3b30f5315cb
Simon <simondereje1@gmail.com>
2024-03-08 17:22:10 +0300

    Classes and Objects Exercise 3

---
+++ Lab-1-Classes-and-Objects-Ex3.dart
@@ -0,0 +1,15 @@
+class Rectangle {
+  final double width;
+  final double height;
+
+  Rectangle(this.width, this.height);
+
+  double calculateArea() {
+    return width * height;
+  }
+
+  double calculatePerimeter() {
+    return 2 * (width + height);
+  }
+}
+
